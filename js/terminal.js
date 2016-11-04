11d1e41b6d3908e40cfe2189773aa0b09a041c54
arianadziedzic <ariana.dziedzic@gmail.com>
2016-11-04 10:37:03 -0400

    add hand assets and button functionality

--- js/terminal.js
+++ js/terminal.js
@@ -1,1 +1,28 @@
 console.log('We are connected!');
+
+const terminal = document.querySelector('#faux-terminal');
+// terminal.scrollTop = terminal.scrollHeight;
+
+function upMovement() {
+  const newPara = document.createElement('p');
+  const command = newPara.innerHTML = 'stick-figure-macbook-air: Arm going up';
+  terminal.appendChild(newPara);
+}
+
+function rightMovement() {
+  const newPara = document.createElement('p');
+  const command = newPara.innerHTML = 'stick-figure-macbook-air: Arm going right';
+  terminal.appendChild(newPara);
+}
+
+function downMovement() {
+  const newPara = document.createElement('p');
+  const command = newPara.innerHTML = 'stick-figure-macbook-air: Arm going down';
+  terminal.appendChild(newPara);
+}
+
+function leftMovement() {
+  const newPara = document.createElement('p');
+  const command = newPara.innerHTML = 'stick-figure-macbook-air: Arm going left';
+  terminal.appendChild(newPara);
+}
